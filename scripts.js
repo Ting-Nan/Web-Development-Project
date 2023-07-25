5066cbe7b331e123f3ccaeea1dedaa64d2044a09
Ting-Nan <michael0170537@gmail.com>
2023-07-25 01:36:16 -0700

    Changed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,37 @@
 function search() {
-    alert("Searching...")
+    const keyword = document.getElementById("searchInput").value.trim();
+
+    if (keyword == "Asia"||keyword =="AsiaPacific") {
+        const url = "AsiaPacific.html?q" + encodeURIComponent(keyword);
+        window.location.href = url;
+        }
+    else if(keyword == "Africa") {
+        const url = "Africa.html?q" + encodeURIComponent(keyword);
+        window.location.href = url;
+        }
+    else if(keyword == "Americas"||keyword =="America") {
+            const url = "Americas.html?q" + encodeURIComponent(keyword);
+            window.location.href = url;
+            }
+    else if(keyword == "Europe") {
+                const url = "Europe.html?q" + encodeURIComponent(keyword);
+                window.location.href = url;
+                }
+     else if(keyword == "NewYork"||keyword =="NewYorkCity") {
+                const url = "NewYorkCity.html?q" + encodeURIComponent(keyword);
+                window.location.href = url;
+                }
+    else if(keyword == "Bangkok"||keyword =="Thailand") {
+                const url = "Bangkok.html?q" + encodeURIComponent(keyword);
+                window.location.href = url;
+                }
+    else if(keyword == "Nairobi"||keyword =="Kenya") {
+                const url = "Nairobi, Kenya.html?q" + encodeURIComponent(keyword);
+                window.location.href = url;
+                }
+    else {
+        alert("No data! Please search again");
+        }
 }
 
 function subscribe() {
