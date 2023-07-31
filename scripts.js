47eb7e70ddf601d104ebed00d3d8da19644c00ad
SaubhagyaM1996 <riligalak@student.douglascollege.ca>
2023-07-30 23:34:48 -0700

    mobile view drop downs and other

--- scripts.js
+++ scripts.js
@@ -37,3 +37,30 @@ function search() {
 function subscribe() {
     alert("Thank you for subscribing! We will send you latest newsletter monthly.")
 }
+
+function showMenu()
+{
+    document.getElementById("maindropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn')) 
+    {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) 
+      {
+        var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) 
+            {
+                openDropdown.classList.remove('show');
+            }
+      }
+    }
+  }
+
+  function showSubMenu()
+  {
+    document.getElementById("subdropdown").classList.toggle("show");
+  }
